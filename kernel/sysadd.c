fc9efb6d63e6d92883305ccc3cd3fa0e7b112b2e
doejoedoe <dokashenko2004@gmail.com>
2026-02-23 13:23:02 +0300

    tasks 1 and 2

---
+++ kernel/sysadd.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "param.h"
+
+uint64 sys_add(void)
+{
+    int a, b;
+    argint(0, &a);
+    argint(1, &b);
+    return (a + b);                      
+}
